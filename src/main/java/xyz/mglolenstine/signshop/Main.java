c92a37257941a18fefc8d4cafe03bf41f204293b
BuildTools <unconfigured@null.spigotmc.org>
2017-07-18 13:39:51 +0200

    Added sign creation and added Vault integration  - Items should be given and deducted now  - Account balance should be changing depending on purchase and/or selling of items.

--- src/main/java/xyz/mglolenstine/signshop/Main.java
+++ src/main/java/xyz/mglolenstine/signshop/Main.java
@@ -1,8 +1,35 @@
 package xyz.mglolenstine.signshop;
 
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  * Created by LifEorDeatH on 17.7.2017.
  */
-public class Main {
+public class Main extends JavaPlugin{
+    static Economy econ;
 
+    public void onEnable() {
+        this.getServer().getPluginManager().registerEvents(new MyListener(), this);
+        if(setupEconomy()){
+            System.out.println(ChatColor.GREEN+"[SignShop] Vault has been successfully initialized");
+        }else{
+            System.out.println(ChatColor.RED+"[SignShop] Vault hasn't been successfully initialized");
+        }
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
 }
--- src/main/java/xyz/mglolenstine/signshop/MyListener.java
+++ src/main/java/xyz/mglolenstine/signshop/MyListener.java
@@ -1,23 +1,68 @@
 package xyz.mglolenstine.signshop;
 
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
 /**
  * Created by LifEorDeatH on 17.7.2017.
  */
 public class MyListener implements Listener {
+    public Economy econ = Main.econ;
+
     @EventHandler
-    void onSignClick(PlayerInteractEvent e){
-        if(e.getClickedBlock().getType() == Material.WALL_SIGN){
+    void onSignClick(PlayerInteractEvent e) {
+        if (e.getClickedBlock().getType() == Material.WALL_SIGN) {
             Sign s = (Sign) e.getClickedBlock();
             String[] text = s.getLines();
-            if(text[0].equals(ChatColor.BLUE+"[SignShop]")){
+            if (text[0].equals(ChatColor.BLUE + "[SignShop]")) {
+                Material mat = Material.getMaterial(text[1]);
+                String[] prices = text[2].split(":");
+                int[] price = {Integer.parseInt(prices[0]), Integer.parseInt(prices[1])};
+                int amount = Integer.parseInt(text[3]);
+                OfflinePlayer op = Bukkit.getOfflinePlayer(e.getPlayer().getUniqueId());
+                if (!econ.hasAccount(op)) {
+                    econ.createPlayerAccount(op);
+                }
+                if (e.getAction() == Action.LEFT_CLICK_BLOCK) {
+                    //Buy action
+                    if (econ.getBalance(op) >= price[0]) {
+                        e.getPlayer().getInventory().addItem(new ItemStack(mat, amount));
+                        econ.withdrawPlayer(op, price[0]);
+                    }else{
+                        e.getPlayer().sendMessage(ChatColor.RED+"[SignShop] You don't have enough money.");
+                    }
+                } else (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+                    //Sell action
+                    if (e.getPlayer().getInventory().contains(mat, amount)) {
+                        e.getPlayer().getInventory().remove(new ItemStack(mat, amount));
+                        econ.depositPlayer(op, price[1]);
+                    }else{
+                        e.getPlayer().sendMessage(ChatColor.RED+"[SignShop] You don't have enough items to sell.");
+                    }
+                }
+            }
+        }
+    }
 
+    @EventHandler
+    void onSignCreate(SignChangeEvent e) {
+        Player p = e.getPlayer();
+        String[] text = e.getLines();
+        if (text[0].equals(ChatColor.BLUE + "[SignShop]")) {
+            if (p.hasPermission("signshop.make")) {
+                e.setLine(0, ChatColor.BLUE + "[SignShop]");
+                ((Sign) e.getBlock()).update();
             }
         }
     }
